d965f458178f91a731e84a7a44f11b3071ba9a41
AdityaCubicles45 <67513926+AdityaCubicles45@users.noreply.github.com>
2024-01-20 19:54:15 +0530

    color error solved

--- library/theme.js
+++ library/theme.js
@@ -1,33 +1,35 @@
 export const COLORS = {
 
-    Black100: 'rgba(244, 244, 244, 1)',
-    Black100: 'rgba(199, 199, 199, 1)',
-    Black100: 'rgba(173, 173, 173, 1)',
-    Black100: 'rgba(148, 148, 148, 1)',
-    Black100: 'rgba(122, 122, 122, 1)',
-    Black100: 'rgba(97, 97, 97, 1)',
-    Black100: 'rgba(71, 71, 71, 1)',
-    Black100: 'rgba(46, 46, 46, 1)',
-    Black100: 'rgba(33, 33, 33, 1)',
-    Black100: 'rgba(27, 27, 27, 1)',
+    Black100: 'rgba(224, 224, 224, 1)',
+    Black200: 'rgba(199, 199, 199, 1)',
+    Black300: 'rgba(173, 173, 173, 1)',
+    Black400: 'rgba(148, 148, 148, 1)',
+    Black500: 'rgba(122, 122, 122, 1)',
+    Black600: 'rgba(97, 97, 97, 1)',
+    Black700: 'rgba(71, 71, 71, 1)',
+    Black800: 'rgba(46, 46, 46, 1)',
+    Black850: 'rgba(33, 33, 33, 1)',
+    Black875: 'rgba(27, 27, 27, 1)',
     Black900Default: 'rgba(20, 20, 20, 1)',
-
+    
     DartmouthGreen100: 'rgba(245, 255, 252, 1)',
-DartmouthGreen600: 'rgba(77, 199, 160, 1)',
-DartmouthGreen800: 'rgba(24, 149, 110, 1)',
-DartmouthGreen900Default: 'rgba(11, 110, 79, 1)',
-
-PersianRed100: 'rgba(255, 245, 246, 1)',
-PersianRed500: 'rgba(209, 87, 97, 1)',
-PersianRed600Default: 'rgba(204, 41, 54, 1)',
-// PersianRed900: 'rgba(90, 1, 9, 1)',
-
-PigmentGreen500: 'rgba(51, 219, 118, 1)',
-PigmentGreen600: 'rgba(22, 197, 92, 1)',
-PigmentGreen700Default: '#08A045',
-PigmentGreen800: '#00752F',
-PigmentGreen900: '#00421B',
-
-StandardWhiteDefault: 'rgba(246, 248, 255, 1)',
-
-};
+    DartmouthGreen600: 'rgba(77, 199, 160, 1)',
+    DartmouthGreen800: 'rgba(24, 149, 110, 1)',
+    DartmouthGreen900Default: 'rgba(11, 110, 79, 1)',
+    
+    PersianRed100: 'rgba(255, 245, 246, 1)',
+    PersianRed500: 'rgba(209, 87, 97, 1)',
+    PersianRed600Default: 'rgba(204, 41, 54, 1)',
+    // PersianRed900: 'rgba(90, 1, 9, 1)',
+    
+    PigmentGreen500: 'rgba(51, 219, 118, 1)',
+    PigmentGreen600: 'rgba(22, 197, 92, 1)',
+    PigmentGreen700Default: '#08A045',
+    PigmentGreen800: '#00752F',
+    PigmentGreen900: '#00421B',
+    
+    StandardWhiteDefault: 'rgba(246, 248, 255, 1)',
+    
+    };
+      
+      
