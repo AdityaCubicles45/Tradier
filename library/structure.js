814cad046b4a7d5cbe95b0e109af4597d9033d13
AdityaCubicles45 <67513926+AdityaCubicles45@users.noreply.github.com>
2024-01-20 19:36:15 +0530

    updated structure groundbreaking

--- library/structure.js
+++ library/structure.js
@@ -2,15 +2,15 @@ import styled from "styled-components";
 import { COLORS } from '@/library/theme'
 import { SIZING } from '@/library/sizing'
 
-export const Section = styled.Section`
+export const Section = styled.section`
 display: flex;
 height: 100vh;
-backgroung-color: ${COLORS.Black900Default};
+background-color: ${COLORS.Black900Default};
 `
 export const ScrollableContainer = styled.section`
 display: flex;
 flex-direction: column;
-width: calc(100w - 20vw);
+width: calc(100vw - 20vw);
 padding-right: ${SIZING.px32};
 padding-bottom: ${SIZING.px32};
 overflow: scroll;
